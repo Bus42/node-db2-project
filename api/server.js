2c65642c4772f13728e63bf5dd71acd516e5cd28
Greg <gbrewton1980@gmail.com>
2021-11-13 19:14:42 -0700

    installs morgan, removes unused dir

--- api/server.js
+++ api/server.js
@@ -1,11 +1,16 @@
 const express = require("express")
 const carsRouter = require("./cars/cars-router");
-const {logger} = require("./middleware/");
+const morgan = require("morgan");
 
 const server = express();
 
-server.use(express.json());
-server.use("/api/cars", logger, carsRouter);
+const middleware = [
+    morgan(":method :url :status :res[content-length] - :response-time ms"),
+    express.json(),
+]
+
+server.use(middleware);
+server.use("/api/cars", carsRouter);
 
 server.get("/", (req, res) => {
     res.status(200).send("server running")
