f8b368e606fbc2db8945d706772819bc60fa5e10
Greg <gbrewton1980@gmail.com>
2021-11-14 14:31:59 -0700

    tests seed migration, clears db

--- data/seeds/01-cars.js
+++ data/seeds/01-cars.js
@@ -1,10 +1,10 @@
-const mockData = require('../mock_data/CARS_MOCK_DATA.js')
+// const mockData = require('../mock_data/CARS_MOCK_DATA.js')
 
 exports.seed = function(knex) {
   // Deletes ALL existing entries
-  return knex('table_name').del()
+  return knex('cars').del()
     .then(function () {
       // Inserts seed entries
-      return knex('table_name').insert(mockData);
+      return knex('cars').insert([{ "vin": "2T1BU4EE8BC386488", "make": "Pontiac", "model": "6000", "mileage": 122676, "title": null, "transmission": "automatic" }]);
     });
 };
