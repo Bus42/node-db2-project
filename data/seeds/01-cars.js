89f65f39341c18576514e87a1515cc8552f8bc05
Greg <gbrewton1980@gmail.com>
2021-11-14 14:35:03 -0700

    creates seed function importing mock data

--- data/seeds/01-cars.js
+++ data/seeds/01-cars.js
@@ -1,10 +1,10 @@
-// const mockData = require('../mock_data/CARS_MOCK_DATA.js')
+const mockData = require('../mock_data/CARS_MOCK_DATA');
 
 exports.seed = function(knex) {
   // Deletes ALL existing entries
   return knex('cars').del()
     .then(function () {
       // Inserts seed entries
-      return knex('cars').insert([{ "vin": "2T1BU4EE8BC386488", "make": "Pontiac", "model": "6000", "mileage": 122676, "title": null, "transmission": "automatic" }]);
+      return knex('cars').insert(mockData);
     });
 };
