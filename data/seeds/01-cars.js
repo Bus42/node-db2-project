8c297e07bacb45a6c778ef215e156ab555611b06
bus42 <gbrewton1980@gmail.com>
2021-11-15 20:07:35 -0700

    Creates seed migration to add 900 records

--- data/seeds/01-cars.js
+++ data/seeds/01-cars.js
@@ -1,10 +1,17 @@
-const mockData = require('../mock_data/CARS_MOCK_DATA_01');
+const mockDataOne = require('../mock_data/CARS_MOCK_DATA_01');
+const mockDataTwo = require('../mock_data/CARS_MOCK_DATA_02');
+const mockDataThree = require('../mock_data/CARS_MOCK_DATA_03');
 
-exports.seed = function(knex) {
+exports.seed = function (knex) {
   // Deletes ALL existing entries
   return knex('cars').del()
     .then(function () {
-      // Inserts seed entries
-      return knex('cars').insert(mockData);
+      return knex('cars').insert(mockDataOne)
+        .then(function () {
+          return knex('cars').insert(mockDataTwo)
+            .then(function () {
+              return knex('cars').insert(mockDataThree)
+            })
+        })
     });
 };
